c6c161fba7bfe125a089a2a6657f137f98423d14
Hann-THL <hann_lim@hotmail.com>
2020-05-07 18:32:48 +0800

    apply customied library

---
+++ python/feature_encoding/lib/_class/DFOrdinalEncoder.py
@@ -0,0 +1,43 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.exceptions import NotFittedError
+import pandas as pd
+
+class DFOrdinalEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self, mapper_dict, handle_unknown=-1, concat_symbol='|'):
+        self.mapper_dict           = mapper_dict
+        self.handle_unknown         = handle_unknown
+        self.concat_symbol         = concat_symbol
+        self.transform_mapper_dict = None
+        
+    def fit(self, X, y=None):
+        self.transform_mapper_dict = {k: v for k,v in self.mapper_dict.items() if k in X.columns}
+        return self
+    
+    def transform(self, X):
+        if self.transform_mapper_dict is None:
+            raise NotFittedError(f"This {self.__class__.__name__} instance is not fitted yet. Call 'fit' with appropriate arguments before using this estimator.")
+
+        new_X = X.copy()
+        for k,v in self.transform_mapper_dict.items():
+            new_X[k] = new_X[k].map(v).fillna(self.handle_unknown)
+
+        return new_X
+    
+    def fit_transform(self, X, y=None):
+        return self.fit(X).transform(X)
+    
+    def inverse_transform(self, X):
+        if self.transform_mapper_dict is None:
+            raise NotFittedError(f"This {self.__class__.__name__} instance is not fitted yet. Call 'fit' with appropriate arguments before using this estimator.")
+
+        new_X = X.copy()
+        for k,v in self.transform_mapper_dict.items():
+            mapper_df  = pd.DataFrame(v.items(), columns=['data', 'encode'])
+            inverse_df = mapper_df.groupby('encode').agg(
+                data=('data', lambda x: self.concat_symbol.join(x))
+            ).reset_index()
+
+            inverse_dict = dict(zip(inverse_df['encode'].values, inverse_df['data'].values))
+            new_X[k] = new_X[k].map(inverse_dict)
+
+        return new_X
